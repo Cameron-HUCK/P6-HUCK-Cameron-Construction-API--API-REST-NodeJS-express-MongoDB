448ef3078dbe6a5c7ed1b90a5d47dddc89903073
DOZCAM <huck-cameron@hotmail.fr>
2022-05-20 16:33:15 +0200

    Page d'acceuil

--- backend/router/sauce.js
+++ backend/router/sauce.js
@@ -10,14 +10,14 @@ const multer = require('../middleware/multer-config');
 
 // CRUD
 // Envoi et Sauvegarde des sauces sur la base de donnee
-router.post('/', sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 // Pour modifier ou mettre a jour le produit
-router.put('/:id', sauceCtrl.modifySauce);
+router.put('/:id', auth, sauceCtrl.modifySauce);
 // Suppression d'une sauce 
-router.delete('/:id', sauceCtrl.deleteSauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
 // Récupération d'une SAUCE spécifique
-router.get('/:id', sauceCtrl.getOneSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 // Renvoie tous les Sauce dans la base de données
-router.get('/', sauceCtrl.getAllSauce);
+router.get('/', auth, sauceCtrl.getAllSauce);
 
 module.exports = router;
